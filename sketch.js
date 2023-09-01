64785e603b9658088e773dbc9eb5bc7182fd87ee
brunaeluiza <143224118+brunaeluiza@users.noreply.github.com>
2023-09-01 17:25:45 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(1000, 1000);
+  background  ("white")
+  }
+  
+     
+     
+     
+  function draw() {
+    rect(0,0,100,100) 
+    fill("red")
+    stroke("black")
+    
+    if(mouseIsPressed){
+      rect(mouseX, mouseY,10,10)}}
